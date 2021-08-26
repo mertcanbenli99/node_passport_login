e8a8708c3412afd9e2252c6da7bbf1b418eea9c7
iammert <mertcanbenli99@outlook.com>
2021-08-27 01:17:34 +0300

    deneme

---
+++ routes/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const {ensureAuthenticated} = require('../config/auth');
+
+const router = express.Router();
+
+
+
+// Welcome PAge 
+router.get('/', (req, res) => res.render('welcome'));
+//Dashboard
+router.get('/dashboard', ensureAuthenticated, (req, res) => 
+res.render('dashboard', {
+   name: req.user.name
+}));
+
+
+module.exports = router;
